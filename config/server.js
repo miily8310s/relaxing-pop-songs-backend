9f791ba683c67f1a99575094ebd8c4ffccfe577a
miily8310s <rillakerika@gmail.com>
2021-05-09 18:53:21 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3eb58868252a847effb2b34e038b3db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3c72067e-1829-499b-a042-bc8c7b7870f5'
+};
